065d21f24e1d2e2e88099272bf2939f7c36314cc
kiran <kiran@hugotechnologies.com>
2014-05-19 14:59:12 +0530

    .

--- mifosng-provider/src/main/java/Exampl.java
+++ mifosng-provider/src/main/java/Exampl.java
@@ -1,18 +1,15 @@
 import java.text.ParseException;
 
-import org.springframework.security.authentication.encoding.PasswordEncoder;
-
 public class Exampl {
 
-	private final PasswordEncoder encoder;
+	//rivate final PasswordEncoder encoder;
   
     public static void main(String[] args) throws ParseException {
 
-				encoder.encodePassword("String", salt);
+				//encoder.encodePassword("String", salt);
 			}
 
 		
 
     }
 
-}
