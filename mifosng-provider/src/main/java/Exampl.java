1a1cf50f0b0194b73678eb8139a0b52025bc4df0
kiran <kiran@hugotechnologies.com>
2014-05-19 12:41:07 +0530

    .

--- mifosng-provider/src/main/java/Exampl.java
+++ mifosng-provider/src/main/java/Exampl.java
@@ -1,18 +1,16 @@
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 
 public class Exampl {
 
+	private final PasswordEncoder encoder;
   
     public static void main(String[] args) throws ParseException {
 
-    	String date="20-May-14";
-    	SimpleDateFormat formatter = new SimpleDateFormat("dd-MMMM-yy");
-    	Date date2=formatter.parse(date);
-    	System.out.println(date2);
-    	   SimpleDateFormat formatter1 = new SimpleDateFormat("dd MMMM yyyy");
-    	   System.out.println(formatter1.format(date2));
+				encoder.encodePassword("String", salt);
+			}
+
 		
 
     }
