a5d5537385ee4bbd39df12afbc38bb6562062b75
kiran <kiran@hugotechnologies.com>
2014-06-20 11:37:07 +0530

    .

--- mifosng-provider/src/main/java/org/mifosplatform/logistics/itemdetails/domain/InventoryItemDetailsRepository.java
+++ mifosng-provider/src/main/java/org/mifosplatform/logistics/itemdetails/domain/InventoryItemDetailsRepository.java
@@ -3,11 +3,12 @@ package org.mifosplatform.logistics.itemdetails.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InventoryItemDetailsRepository extends JpaRepository<InventoryItemDetails, Long>, JpaSpecificationExecutor<InventoryItemDetails>{
 
 	@Query("from InventoryItemDetails item where item.serialNumber = :macId")
-	InventoryItemDetails getInventoryItemDetailBySerialNum(String macId);
+	InventoryItemDetails getInventoryItemDetailBySerialNum(@Param("macId") String macId);
 
 }
 
