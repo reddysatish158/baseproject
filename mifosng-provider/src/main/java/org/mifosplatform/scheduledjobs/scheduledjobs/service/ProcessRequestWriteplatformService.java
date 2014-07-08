018825d440307697a6241c9af94e23fc06b41647
kiran <kiran@hugotechnologies.com>
2014-07-08 12:52:58 +0530

    .

--- mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/scheduledjobs/service/ProcessRequestWriteplatformService.java
+++ mifosng-provider/src/main/java/org/mifosplatform/scheduledjobs/scheduledjobs/service/ProcessRequestWriteplatformService.java
@@ -1,12 +1,11 @@
 package org.mifosplatform.scheduledjobs.scheduledjobs.service;
 
-import org.mifosplatform.provisioning.processrequest.data.ProcessingDetailsData;
 import org.mifosplatform.provisioning.processrequest.domain.ProcessRequest;
 
 public interface ProcessRequestWriteplatformService {
 
 	void ProcessingRequestDetails();
 
-	void notifyProcessingDetails(ProcessRequest processRequest);
+	void notifyProcessingDetails(ProcessRequest processRequest, char status);
 
 }
