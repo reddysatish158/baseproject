0f89261a10abcbfaf8c7af66332adc57afc7180e
Ranjith15 <ranjithworld150@gmail.com>
2014-07-30 20:03:29 +0530

    .

---
+++ mifosng-db/migrations/core_db/V122__add_messageTemplate_data .sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO b_message_template VALUES(null, 'TICKET_TEMPLATE', 'TICKET', 'TICKET', 'create ticket', '', 'E', '1', NULL, NULL, NULL, 'N');
+
+
+INSERT IGNORE INTO b_message_template VALUES(null,'Bill_Email', 'TAX INVOICE from Spicent', 'Bill', 'Please find the attached TAX INVOCIE from Spicenet', 'Thanks', 'E', '1', '2014-07-30 12:33:42', '1', '2014-07-30 12:33:42', 'N');
+
