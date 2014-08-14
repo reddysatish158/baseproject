6f8d5d93cefe72672d92d828d3a378114f376aa2
reddysatish158 <reddysatish158@gmail.com>
2014-08-14 18:18:55 +0530

    .

--- mifosng-db/migrations/core_db/V121__add_alter_tables_grn_item_masterandpermission.sql
+++ mifosng-db/migrations/core_db/V121__add_alter_tables_grn_item_masterandpermission.sql
@@ -9,13 +9,16 @@ Begin
      and TABLE_NAME = 'b_item_master'
      and TABLE_SCHEMA = DATABASE())THEN
 Alter table b_item_master add `reorder_level` int(2) DEFAULT NULL;
+end if;
 END $$
+delimiter ;
 call addreorderlevel();
+Drop procedure IF EXISTS addreorderlevel;
 
 INSERT IGNORE INTO `m_permission`(grouping,code,entity_name,action_name,can_maker_checker) 
 VALUES ('logistics','UPDATE_GRN','GRN','UPDATE',1);
 
-Drop procedure IF EXISTS addreorderlevel;
+
 
 DELIMITER $$
 Drop procedure IF EXISTS addpono $$
@@ -27,6 +30,9 @@ Begin
      and TABLE_NAME = 'b_grn'
      and TABLE_SCHEMA = DATABASE())THEN
 ALTER table b_grn add `po_no` varchar(20) DEFAULT NULL;
+end if;
 END $$
+delimiter ;
 call addpono();
+Drop procedure IF EXISTS addpono ;
 
